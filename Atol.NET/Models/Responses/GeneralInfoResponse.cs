9a1faa36bd1201836585bc1a616865787ff114a6
Tamerlan <tamikappaev@yandex.ru>
2022-07-14 02:04:24 +0300

    Add GeneralInfoResponse.cs model and ResponseBase.cs model

---
+++ Atol.NET/Models/Responses/GeneralInfoResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Atol.NET.Models.Responses;
+
+public class GeneralInfoResponse : ResponseBase
+{
+    
+}
---
+++ Atol.NET/Models/Responses/ResponseBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Atol.NET.Models.Responses;
+
+
+public class ResponseBase
+{
+    public bool IsSuccess { get; set; }
+    
+    public int ErrorCode { get; set; }
+    
+    public string? Description { get; set; }
+}
