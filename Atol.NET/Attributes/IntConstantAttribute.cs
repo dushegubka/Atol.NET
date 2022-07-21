746cf8e05225a8b703aa472e9228c36caf98b363
Tamerlan <tamikappaev@yandex.ru>
2022-07-21 19:06:15 +0300

    Добавляет атрибуты для привязки айди константы драйвера к свойствам моделей данных

---
+++ Atol.NET/Attributes/IntConstantAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Atol.NET.Attributes;
+
+public class IntConstantAttribute : ParamBaseAttribute
+{
+    public IntConstantAttribute(int constant, Type returningType) : base(returningType)
+    {
+        Constant = constant;
+    }
+
+    public int Constant { get; }
+}
---
+++ Atol.NET/Attributes/ParamBaseAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Atol.NET.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ParamBaseAttribute : Attribute
+{
+    protected ParamBaseAttribute(Type returningType)
+    {
+        ReturningType = returningType;
+    }
+    
+    public Type ReturningType { get; }
+}
