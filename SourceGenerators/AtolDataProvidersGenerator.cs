76ae7f18f541cdf83bf7cd57c323cb30ad3bf791
Tamerlan <tamikappaev@yandex.ru>
2022-07-21 19:08:21 +0300

    Добавляет в проект сурс-генератор IAtolDataProvider для разных типов возвращаемых данных

---
+++ SourceGenerators/AtolDataProvidersGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGenerators
+{
+    [Generator]
+    public class AtolDataProvidersGenerator : ISourceGenerator
+    {
+        private List<Type> _types;
+        private Dictionary<string, string> _methodsNames;
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            _types = new List<Type>
+            {
+                typeof(int),
+                typeof(string),
+                typeof(DateTime),
+                typeof(double),
+                typeof(bool),
+                typeof(byte[])
+            };
+
+            _methodsNames = new Dictionary<string, string>
+            {
+                {"Int32", "getParamInt"},
+                {"String", "getParamString"},
+                {"DateTime", "getParamDateTime"},
+                {"Double", "getParamDouble"},
+                {"Boolean", "getParamBool"},
+                {"Byte[]", "getParamByteArray"}
+            };
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            foreach (var type in _types)
+            {
+                var className = $"{GetLettersFromString(type.Name)}AtolDataProvider";
+                    var source = string.Format(@"using System;
+using System.Collections.Generic;
+using Atol.Drivers10.Fptr;
+using Atol.NET.Abstractions;
+
+namespace Atol.NET.DataProviders
+{{
+    public class {0} : IAtolDataProvider
+    {{
+        private readonly IFptr _kkt;
+        
+        public {0}(IFptr kkt)
+        {{
+            _kkt = kkt;
+        }}
+
+        public Type GetResultType() => typeof({1});
+
+        public object GetData(int constant)
+        {{
+            return _kkt.{2}(constant);
+        }}
+    }}
+}}", className, type, _methodsNames[type.Name]);
+                context.AddSource($"{className}.g.cs", SourceText.From(source, Encoding.UTF8));
+            }
+        }
+
+        private string GetLettersFromString(string input)
+        {
+            return new string(input.Where(c => char.IsLetter(c)).ToArray());
+        }
+    }
+}
