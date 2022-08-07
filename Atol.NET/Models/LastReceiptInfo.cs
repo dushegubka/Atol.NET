6e456a1c9af2aa018eda51aaf343a83d99e8d08a
Tamerlan <tamikappaev@yandex.ru>
2022-08-08 01:08:34 +0300

    Добавляет FiscalStorageInfo.cs и LastReceiptInfo.cs

---
+++ Atol.NET/Models/LastReceiptInfo.cs
@@ -0,0 +1,23 @@
+﻿using Atol.Drivers10.Fptr;
+using Atol.NET.Attributes;
+using Atol.NET.Enums;
+
+namespace Atol.NET.Models;
+
+public class LastReceiptInfo
+{
+    [IntConstant(Constants.LIBFPTR_PARAM_DOCUMENT_NUMBER, typeof(int))]
+    public int DocumentNumber { get; set; }
+
+    [IntConstant(Constants.LIBFPTR_PARAM_RECEIPT_TYPE, typeof(int))]
+    public ReceiptType ReceiptType { get; set; }
+    
+    [IntConstant(Constants.LIBFPTR_PARAM_RECEIPT_SUM, typeof(double))]
+    public double Sum { get; set; }
+    
+    [IntConstant(Constants.LIBFPTR_PARAM_FISCAL_SIGN, typeof(string))]
+    public string? FiscalSign { get; set; }
+
+    [IntConstant(Constants.LIBFPTR_PARAM_DATE_TIME, typeof(DateTime))]
+    public DateTime DateTime { get; set; }
+}
