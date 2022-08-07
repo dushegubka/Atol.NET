52b691dafcceb298e07a5dbdebcf2747a040af72
Tamerlan <tamikappaev@yandex.ru>
2022-08-07 20:58:25 +0300

    Добавляет AtolException.cs

---
+++ Atol.NET/Exceptions/AtolException.cs
@@ -0,0 +1,10 @@
+﻿namespace Atol.NET.Exceptions;
+
+public class AtolException : Exception
+{
+    public AtolException(string errorMessage, int errorCode) : base(errorMessage)
+    {
+        ErrorCode = errorCode;
+    }
+    public int ErrorCode { get;}
+}
