9798a3a66e2d232f53e8516761806735c497fb05
Tamerlan <tamikappaev@yandex.ru>
2022-08-08 01:35:58 +0300

    Добавил классы LastDocumentInfo.cs и ShiftInfo.cs

---
+++ Atol.NET/Models/LastDocumentInfo.cs
@@ -0,0 +1,25 @@
+﻿using Atol.Drivers10.Fptr;
+using Atol.NET.Attributes;
+
+namespace Atol.NET.Models;
+
+public class LastDocumentInfo
+{
+    /// <summary>
+    /// Номер документа
+    /// </summary>
+    [Constant(Constants.LIBFPTR_PARAM_DOCUMENT_NUMBER, typeof(int))]
+    public int DocumentNumber { get; set; }
+
+    /// <summary>
+    /// Фискальный признак документа
+    /// </summary>
+    [Constant(Constants.LIBFPTR_PARAM_FISCAL_SIGN, typeof(string))]
+    public string? FiscalSign { get; set; }
+
+    /// <summary>
+    /// Дата и время документа
+    /// </summary>
+    [Constant(Constants.LIBFPTR_PARAM_DATE_TIME, typeof(DateTime))]
+    public DateTime DateTime { get; set; }
+}
---
+++ Atol.NET/Models/ShiftInfo.cs
@@ -0,0 +1,19 @@
+﻿using Atol.Drivers10.Fptr;
+using Atol.NET.Attributes;
+
+namespace Atol.NET.Models;
+
+public class ShiftInfo
+{
+    /// <summary>
+    /// Количество чеков за смену
+    /// </summary>
+    [Constant(Constants.LIBFPTR_PARAM_RECEIPT_NUMBER, typeof(int))]
+    public int ReceiptNumber { get; set; }
+    
+    /// <summary>
+    /// Номер смены
+    /// </summary>
+    [Constant(Constants.LIBFPTR_PARAM_SHIFT_NUMBER, typeof(int))]
+    public int ShiftNumber { get; set; }
+}
