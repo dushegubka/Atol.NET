d2b7531c137dae4974cccba6a24c05c58dd14f8d
Tamerlan <tamikappaev@yandex.ru>
2022-08-18 20:19:10 +0300

    Добавляет IFluentConnector.cs

---
+++ Atol.NET/Abstractions/Connectors/IFluentConnector.cs
@@ -0,0 +1,12 @@
+﻿namespace Atol.NET.Abstractions.Connectors;
+
+public interface IFluentConnector
+{
+    ITcpIpConnector TcpIp { get; }
+    
+    IBluetoothConnector Bluetooth { get; }
+    
+    IComConnector Com { get; }
+    
+    IUsbConnector Usb { get; }
+}
