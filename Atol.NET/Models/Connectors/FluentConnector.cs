7441f321ba355389045e65626ebda557e31d6b7b
Tamerlan <tamikappaev@yandex.ru>
2022-08-18 20:22:11 +0300

    Добавляет реализацию fluent-интерфейса для подключения к ККТ

---
+++ Atol.NET/Models/Connectors/FluentConnector.cs
@@ -0,0 +1,23 @@
+﻿using Atol.Drivers10.Fptr;
+using Atol.NET.Abstractions;
+using Atol.NET.Abstractions.Connectors;
+
+namespace Atol.NET.Models.Connectors;
+
+public class FluentConnector : IFluentConnector
+{
+    public FluentConnector(IFptr kkt, IKktRequestService requestService)
+    { 
+        TcpIp = new TcpIpConnector(kkt, requestService);
+        Bluetooth = new BluetoothConnector(kkt, requestService);
+        Com = new ComConnector(kkt, requestService);
+        Usb = new UsbConnector(kkt, requestService);
+    }
+    public ITcpIpConnector TcpIp { get; }
+    
+    public IBluetoothConnector Bluetooth { get; }
+    
+    public IComConnector Com { get; }
+    
+    public IUsbConnector Usb { get; }
+}
