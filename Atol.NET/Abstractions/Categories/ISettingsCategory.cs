2b6cca4f8badf3dbe54a668ba6257b224fa2d522
dotNevereverlie <tamikappaev@yandex.ru>
2022-10-28 15:30:12 +0300

    Добавил ISettingsCategory.cs и его дефолтную реализацию SettingsCategory.cs

---
+++ Atol.NET/Abstractions/Categories/ISettingsCategory.cs
@@ -0,0 +1,8 @@
+﻿using Atol.NET.Models.Responses;
+
+namespace Atol.NET.Abstractions.Categories;
+
+public interface ISettingsCategory
+{
+    KktBaseResponse ShowSettingsWindow(IntPtr descriptor);
+}
---
+++ Atol.NET/Categories/SettingsCategory.cs
@@ -0,0 +1,26 @@
+﻿using Atol.Drivers10.Fptr;
+using Atol.NET.Abstractions;
+using Atol.NET.Abstractions.Categories;
+using Atol.NET.Models.Responses;
+
+namespace Atol.NET.Categories;
+
+public class SettingsCategory : ISettingsCategory
+{
+    private readonly IFptr _kkt;
+    private readonly IKktRequestService _requestService;
+
+    public SettingsCategory(IFptr kkt, IKktRequestService requestService)
+    {
+        _kkt = kkt;
+        _requestService = requestService;
+    }
+    
+    public KktBaseResponse ShowSettingsWindow(IntPtr descriptor)
+    {
+        return _requestService.SendRequest(() =>
+        {
+            _kkt.showProperties(Constants.LIBFPTR_GUI_PARENT_NATIVE, descriptor);
+        });
+    }
+}
