72cf337aea03741aa6596c17085f0a3330206877
dotNevereverlie <tamikappaev@yandex.ru>
2022-11-08 17:42:02 +0300

    Добавил флаги TaxSystem и AgentSign

---
+++ Atol.NET/Enums/AgentSign.cs
@@ -0,0 +1,45 @@
+namespace Atol.NET.Enums;
+
+[Flags]
+public enum AgentSign
+{
+    /// <summary>
+    /// Признак агента отсутствует
+    /// </summary>
+    None = 0,
+    
+    /// <summary>
+    /// Банковский платежный агент
+    /// </summary>
+    BankPayingAgent = 1,
+    
+    /// <summary>
+    /// Банковский платежный субагент
+    /// </summary>
+    BankPayingSubagent = 2,
+    
+    /// <summary>
+    /// Платежный агент
+    /// </summary>
+    PayingAgent = 4,
+    
+    /// <summary>
+    /// Платежный субагент
+    /// </summary>
+    PayingSubagent = 8,
+    
+    /// <summary>
+    /// Поверенный
+    /// </summary>
+    Attorney = 16,
+    
+    /// <summary>
+    /// Комиссионер
+    /// </summary>
+    CommissionAgent = 32,
+    
+    /// <summary>
+    /// Другой тип агента, "иной" агент
+    /// </summary>
+    Another = 64
+}
---
+++ Atol.NET/Enums/TaxSystem.cs
@@ -0,0 +1,30 @@
+namespace Atol.NET.Enums;
+
+[Flags]
+public enum TaxSystem
+{
+    /// <summary>
+    /// Общая система налогообложения
+    /// </summary>
+    Osn = 1,
+    
+    /// <summary>
+    /// Упрощенная система налогообложения (доход)
+    /// </summary>
+    UsnIncome = 2,
+    
+    /// <summary>
+    /// Упрощенная система налогообложения (доход минус расход)
+    /// </summary>
+    UsnIncomeOutcome = 4,
+    
+    /// <summary>
+    /// Единый сельскохозяйственный доход
+    /// </summary>
+    Esn = 16,
+    
+    /// <summary>
+    /// Патентная система налогообложения
+    /// </summary>
+    Patent = 32
+}
