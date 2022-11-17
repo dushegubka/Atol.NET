4b1657e58e308bff49c8fc1bff578357d7552a80
dotNevereverlie <tamikappaev@yandex.ru>
2022-11-18 01:13:34 +0300

    Добавил перечисление ReportType.cs (разбитие класса Constants.cs)

---
+++ Atol.NET/Enums/ReportType.cs
@@ -0,0 +1,28 @@
+namespace Atol.NET.Enums;
+
+public enum ReportType
+{
+    X,
+    LastDocument,
+    OfdExchangeStatus,
+    KktDemo,
+    KktInfo,
+    OfdTest,
+    FnDocByNumber,
+    Quantity,
+    Departments,
+    Operators,
+    Hours,
+    FnRegistrations,
+    FnShiftTotalCounters,
+    FnTotalCounters,
+    FnNotSentDocumentsCounters,
+    CommoditiesByTaxationTypes,
+    CommoditiesByDepartments,
+    CommoditiesBySums,
+    StartService,
+    Discounts,
+    JournalDocumentByNumbers,
+    JournalDocumentByShifts,
+    CloseShiftReports
+}
